2e1f89e2d7e9ae1b5e62a3d4ae077f08835a275f
unknown <phucp9698@gmail.com>
2026-05-07 16:53:47 +0700

    deploy vps

---
+++ src/main/java/com/phucitdev/pickleball_backend/commo/config/openapi/OpenApiConfig.java
@@ -0,0 +1,21 @@
+package com.phucitdev.pickleball_backend.commo.config.openapi;
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .servers(List.of(
+                        new Server().url("https://crm.singles")
+                ));
+    }
+}
